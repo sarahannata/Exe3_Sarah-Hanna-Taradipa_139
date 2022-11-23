db0e19e8730047b631978756ce114894c2505145
sarahannata <sarahannata@gmail.com>
2022-11-23 15:53:08 +0700

    Membuat public void firstNode()

--- Exe3_Sarah Hanna Taradipa_139/Program.cs
+++ Exe3_Sarah Hanna Taradipa_139/Program.cs
@@ -62,5 +62,13 @@ namespace Exercise_Linked_list_A
                 Console.Write(LAST.rollNumber + "   " + LAST.name + "\n");
             }
         }
+        public void  firstNode()
+        {
+            if (listEmpty())
+                Console.WriteLine("\nList is empty");
+            else
+                Console.WriteLine("\nThe first record in the list is:\n\n " +
+                    LAST.next.rollNumber + "   " + LAST.next.name);
+        }
     }
 }
