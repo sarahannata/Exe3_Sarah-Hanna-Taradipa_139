bd144e99006de39ff37d8f60055c8f4712c9005b
sarahannata <sarahannata@gmail.com>
2022-11-23 15:28:38 +0700

    Add project files.

---
+++ Exe3_Sarah Hanna Taradipa_139/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exe3_Sarah_Hanna_Taradipa_139
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
